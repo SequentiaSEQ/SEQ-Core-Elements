9f3327f8db6ae4c61c04106754a77507f8656896
JBetz <joebetz91@gmail.com>
2024-03-28 05:58:10 +0100

    Use JSON rather than INI config file for asset exchange rates

--- src/exchangerates.cpp
+++ src/exchangerates.cpp
@@ -1,19 +1,19 @@
 #include <asset.h>
 #include <assetsdir.h>
-#include <boost/program_options.hpp>
-#include <boost/program_options/variables_map.hpp>
 #include <consensus/amount.h>
 #include <cstdint>
 #include <exchangerates.h>
 #include <fs.h>
+#include <fstream>
 #include <policy/policy.h>
 #include <uint256.h>
+#include <univalue.h>
 
 // TODO: Do we need a lock to protect this?
 std::map<CAsset, CAssetExchangeRate> g_exchange_rate_map = {};
 
 CAmount CalculateExchangeValue(const CAmount& amount, const CAsset& asset) {
-    auto it = g_exchange_rate_map.find(asset); 
+    auto it = g_exchange_rate_map.find(asset);
     if (it == g_exchange_rate_map.end()) {
         return 0;
     }
@@ -27,22 +27,47 @@ CAmount CalculateExchangeValue(const CAmount& amount, const CAsset& asset) {
     }
 }
 
-bool LoadExchangeRatesFromConfigFile(std::string file_path, std::string& error)
+bool LoadExchangeRatesFromJSONFile(std::string file_path, std::string& error)
 {
-    // Parse config file
-    boost::program_options::variables_map options;
-    boost::program_options::options_description options_description;
-    boost::program_options::store(boost::program_options::parse_config_file(file_path.c_str(), options_description), options);
-    boost::program_options::notify(options);
+    // Read config file
+    std::ifstream ifs(file_path);
+    if (!ifs.is_open()) {
+        error = "Failed to open file";
+        return false;
+    }
+    std::stringstream buffer;
+    buffer << ifs.rdbuf();
+
+    // Parse as JSON
+    std::string rawJson = buffer.str();
+    UniValue json;
+    if (!json.read(rawJson)) {
+        error = "Cannot parse JSON";
+        return false;
+    }
+    std::map<std::string, UniValue> assetMap;
+    json.getObjMap(assetMap);
 
     // Load exchange rates into global map
-    for (auto option : options) {
-        CAsset asset = GetAssetFromString(option.first);
+    for (auto assetEntry : assetMap) {
+        auto assetIdentifier = assetEntry.first;
+        auto assetData = assetEntry.second;
+        CAsset asset = GetAssetFromString(assetIdentifier);
         if (asset.IsNull()) {
             error = strprintf("Unknown label and invalid asset hex: %s", asset.GetHex());
             return false;
         }
-        CAmount exchangeRateValue = option.second.as<int>();
+        CAmount exchangeRateValue;
+        if (assetData.isNum()) {
+            exchangeRateValue = assetData.get_int();
+        } else if (assetData.isObject()) {
+            std::map<std::string, UniValue> assetFields;
+            assetData.getObjMap(assetFields);
+            exchangeRateValue = assetFields["value"].get_int();
+        } else {
+            error = strprintf("Invalid value for asset %s: %d", assetIdentifier, assetData.getValStr());
+            return false;
+        }
         g_exchange_rate_map[asset] = exchangeRateValue;
     }
     return true;
