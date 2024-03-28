9c22ad12d22f01b5193c9a9fafa10053ed6be192
JBetz <joebetz91@gmail.com>
2024-03-28 03:50:42 +0100

    Add option to load exchange rates from config file

--- src/exchangerates.cpp
+++ src/exchangerates.cpp
@@ -1,7 +1,11 @@
 #include <asset.h>
+#include <assetsdir.h>
+#include <boost/program_options.hpp>
+#include <boost/program_options/variables_map.hpp>
 #include <consensus/amount.h>
 #include <cstdint>
 #include <exchangerates.h>
+#include <fs.h>
 #include <policy/policy.h>
 #include <uint256.h>
 
@@ -21,4 +25,25 @@ CAmount CalculateExchangeValue(const CAmount& amount, const CAsset& asset) {
     } else {
         return (uint64_t) value;
     }
+}
+
+bool LoadExchangeRatesFromConfigFile(std::string file_path, std::string& error)
+{
+    // Parse config file
+    boost::program_options::variables_map options;
+    boost::program_options::options_description options_description;
+    boost::program_options::store(boost::program_options::parse_config_file(file_path.c_str(), options_description), options);
+    boost::program_options::notify(options);
+
+    // Load exchange rates into global map
+    for (auto option : options) {
+        CAsset asset = GetAssetFromString(option.first);
+        if (asset.IsNull()) {
+            error = strprintf("Unknown label and invalid asset hex: %s", asset.GetHex());
+            return false;
+        }
+        CAmount exchangeRateValue = option.second.as<int>();
+        g_exchange_rate_map[asset] = exchangeRateValue;
+    }
+    return true;
 }
