de1eed53c82ba91ed594b9020f32cee64f607759
JBetz <joebetz91@gmail.com>
2024-05-02 13:23:17 +0200

    Add tests for paytxfee parameter

--- test/functional/feature_any_asset_fee_rates.py
+++ test/functional/feature_any_asset_fee_rates.py
@@ -14,10 +14,10 @@ from test_framework.util import (
 class AnyAssetFeeRatesTest(BitcoinTestFramework):
     def set_test_params(self):
         self.setup_clean_chain = True
-        self.num_nodes = 2
+        self.num_nodes = 4
         self.base_args = [
             "-blindedaddresses=1",
-            "-initialfreecoins=1000000000",
+            "-initialfreecoins=100000000000",
             "-con_blocksubsidy=0",
             "-con_connect_genesis_outputs=1",
             "-con_any_asset_fees=1",
@@ -26,7 +26,9 @@ class AnyAssetFeeRatesTest(BitcoinTestFramework):
         ]
         self.extra_args = [
             self.base_args + ["-mintxfee=0.001"],
-            self.base_args + ["-mintxfee=0.002"]]
+            self.base_args + ["-mintxfee=0.002"],
+            self.base_args + ["-paytxfee=0.003"],
+            self.base_args + ["-paytxfee=0.004"]]
         self.extra_args[0].append("-anyonecanspendaremine=1")
 
     def skip_test_if_missing_module(self):
@@ -49,17 +51,15 @@ class AnyAssetFeeRatesTest(BitcoinTestFramework):
         self.nodes[0].generatetoaddress(1, self.nodes[0].getnewaddress(), invalid_call=False)  # confirm the tx
 
         self.issuance_addr = self.nodes[0].gettransaction(self.issuance_txid)['details'][0]['address']
-        self.nodes[1].importaddress(self.issuance_addr)
-        issuance_key = self.nodes[0].dumpissuanceblindingkey(self.issuance_txid, self.issuance_vin)
-        self.nodes[1].importissuanceblindingkey(self.issuance_txid, self.issuance_vin, issuance_key)
-
-        self.node0_address = self.nodes[0].getnewaddress()
-        self.node1_address = self.nodes[1].getnewaddress()
+        issuance_key = self.nodes[0].dumpissuanceblindingkey(self.issuance_txid, self.issuance_vin)        
 
         new_rates = { "gasset": 100000000, self.asset: 200000000 }
-        self.nodes[0].setfeeexchangerates(new_rates)
-        self.nodes[1].setfeeexchangerates(new_rates)
-
+        for node in self.nodes:
+            node.address = node.getnewaddress()
+            node.importaddress(self.issuance_addr)
+            node.importissuanceblindingkey(self.issuance_txid, self.issuance_vin, issuance_key)
+            node.setfeeexchangerates(new_rates)
+            
     def test_fee_rates(self):
         # Fee rate parameter
         self.node = self.nodes[0]
@@ -71,29 +71,41 @@ class AnyAssetFeeRatesTest(BitcoinTestFramework):
         self.test_fund_fee(self.asset, 0.1, Decimal('0.00124550'))
 
         # Now test node1, which has a higher minimum tx fee
-        self.fund_node1()
         self.node = self.nodes[1]
+        self.fund_node(self.node)
         self.test_fund_fee(self.gasset, 0.1, Decimal('0.00498200'))
         self.test_fund_fee(self.asset, 0.1, Decimal('0.00249100'))
 
+        # Now test node2, which has a higher paytxfee
+        self.node = self.nodes[2]
+        self.fund_node(self.node)
+        self.test_fund_fee(self.gasset, 0.1, Decimal('0.00747300'))
+        self.test_fund_fee(self.asset, 0.1, Decimal('0.00373650'))
+
+        # Now test node3, which has a (still) higher paytxfee
+        self.node = self.nodes[3]
+        self.fund_node(self.node)
+        self.test_fund_fee(self.gasset, 0.1, Decimal('0.00996400'))
+        self.test_fund_fee(self.asset, 0.1, Decimal('0.00498200'))
+
     def test_fund_fee(self, asset, amount, expected_fee, options=None):
         raw_tx = self.node.createrawtransaction(outputs=[
-            {self.node0_address: amount, 'asset': asset},
+            {self.nodes[0].address: amount, 'asset': asset},
             {'fee': 0,'fee_asset': asset}])
         funded_tx = self.node.fundrawtransaction(hexstring=raw_tx, options=options)['hex']
         tx = self.node.decoderawtransaction(funded_tx)
         assert_equal(tx['fee'], {asset: expected_fee})
 
-    def fund_node1(self):
+    def fund_node(self, node):
         self.nodes[0].sendtoaddress(
-            address=self.node1_address,
+            address=node.address,
             amount=5.0,
             assetlabel=self.gasset)
         self.nodes[0].sendtoaddress(
-            address=self.node1_address,
+            address=node.address,
             amount=5.0,
             assetlabel=self.asset)
-        self.generatetoaddress(self.nodes[0], 1, self.node0_address)
+        self.generatetoaddress(self.nodes[0], 1, self.nodes[0].address)
         self.sync_all()
 
     def run_test(self):
